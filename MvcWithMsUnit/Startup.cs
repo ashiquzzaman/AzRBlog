53a44f8994c01f53878023c46326415e95197de4
ashiquzzaman <md.ashiquzzaman@datasoft-bd.com>
2018-02-12 09:20:59 +0600

    Add project files.

---
+++ MvcWithMsUnit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcWithMsUnit.Startup))]
+namespace MvcWithMsUnit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
