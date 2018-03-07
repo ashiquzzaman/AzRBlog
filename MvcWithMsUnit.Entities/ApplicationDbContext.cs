c3b402904a3bca0a114f37077bfe21cfb4087c4b
ashiquzzaman <md.ashiquzzaman@datasoft-bd.com>
2018-03-07 17:24:12 +0600

    override SaveChangesAsync method

--- MvcWithMsUnit.Entities/ApplicationDbContext.cs
+++ MvcWithMsUnit.Entities/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace MvcWithMsUnit.Entities
 {
@@ -57,6 +58,40 @@ namespace MvcWithMsUnit.Entities
             }
 
             return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync()
+        {
+
+            var modifiedEntries = ChangeTracker.Entries()
+                .Where(x => x.Entity is IAuditableEntity
+                            && (x.State == System.Data.Entity.EntityState.Added ||
+                                x.State == System.Data.Entity.EntityState.Modified));
+
+            foreach (var entry in modifiedEntries)
+            {
+                IAuditableEntity entity = entry.Entity as IAuditableEntity;
+                if (entity != null)
+                {
+                    string identityName = Thread.CurrentPrincipal.Identity.Name;
+                    DateTime now = DateTime.UtcNow;
+
+                    if (entry.State == System.Data.Entity.EntityState.Added)
+                    {
+                        entity.CreatedBy = identityName;
+                        entity.CreatedDate = now;
+                    }
+                    else
+                    {
+                        base.Entry(entity).Property(x => x.CreatedBy).IsModified = false;
+                        base.Entry(entity).Property(x => x.CreatedDate).IsModified = false;
+                    }
+
+                    entity.UpdatedBy = identityName;
+                    entity.UpdatedDate = now;
+                }
+            }
+            return base.SaveChangesAsync();
         }
 
 
